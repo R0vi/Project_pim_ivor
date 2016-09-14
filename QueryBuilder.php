965adcc8727847f300e6693c5a799812f45753fa
Ivor Staats <ivor.staats@student.aventus.nl>
2016-09-14 17:02:19 +0200

    start select query builder

--- QueryBuilder.php
+++ QueryBuilder.php
@@ -3,14 +3,21 @@
 class QueryBuilder
 {
     private $connection;
+    private $db;
 
     public function __construct(Connection $connection)
     {
         $connection = $this->connection;
+        $this->db = $this->connection->db;
     }
 
-    public function selectQuery($table, $data)
+    public function selectQuery($table, $data, $where = '')
     {
-        
+        $whereTag = 'WHERE';
+        if ($where == '')
+        {
+            $whereTag == '';
+        }
+        $this->db->prepare('SELECT '.$data.'FROM '.$table.' '.$whereTag.' '.$where.';');
     }
 }
--- connection.php
+++ connection.php
@@ -1,13 +1,19 @@
 <?php
 
-class Connection extends \PDO
+class Connection
 {
     private $database;
     private $host;
     private $db;
 
-    public function raw($query)
+    public function __construct($host = 'localhost', $database)
     {
-        $this->prepare($query);
+        $this->database = $database;
+        $this->host = $host;
+    }
+
+    public function connection($username, $password)
+    {
+        $this->dbh = new PDO('mysql:host='.$this->host.';dbname='.$this->database, $username, $password);
     }
 }
